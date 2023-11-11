8dc87f1c5578538ff5cf6d36f8e0974f2ce36059
Myeongpyo Hong <hmpjp23@gmail.com>
2023-11-11 21:01:11 +0900

    fcuser까지 작업

--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -3,13 +3,11 @@ from django.db import models
 # Create your models here.
 
 
-class Fcuser(models.Model):
-    username = models.CharField(max_length=64,
-                                verbose_name='사용자명')
-    password = models.CharField(max_length=64,
-                                verbose_name='비밀번호')
-    registered_dttm = models.DateTimeField(auto_now_add=True,  # 현재 시간 자동 등록
-                                           verbose_name='등록시간')
+class Fcuser(models.Model):  # 객체
+    username = models.CharField(max_length=64, verbose_name='사용자명')
+    password = models.CharField(max_length=64, verbose_name='비밀번호')
+    registered_dttm = models.DateTimeField(
+        auto_now_add=True,  verbose_name='등록시간')  # 현재 시간 자동 등록
 
     class Meta:
         db_table = 'fastcampus_fcuser'
