24dbb86110fc56d018378387fcbc6d49c0bfcde7
Myeongpyo Hong <hmpjp23@gmail.com>
2023-11-11 22:04:41 +0900

    fcuser의 model 마이그레이션

--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Fcuser(models.Model):  # 객체
-    username = models.CharField(max_length=64, verbose_name='사용자명')
+    username = models.CharField(max_length=32, verbose_name='사용자명')
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(
         auto_now_add=True,  verbose_name='등록시간')  # 현재 시간 자동 등록
