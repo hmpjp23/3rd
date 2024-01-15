1d276d468699a3a3891568b2a04b579ba7d3b11c
홍명표 <hmpjp@hmpjpui-MacBookPro.local>
2024-01-15 22:50:02 +0900

    admin 개발

--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -8,6 +8,12 @@ class Fcuser(models.Model):  # 객체
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(
         auto_now_add=True,  verbose_name='등록시간')  # 현재 시간 자동 등록
+    
+
+    def __str__(self): #class를 문자열로 변환할때 내장 함수인 __str__를 호출한다. 이를 이용해서 return을 정의해줌
+        return self.username
 
     class Meta:
         db_table = 'fastcampus_fcuser'
+        verbose_name = '패스트캠퍼스 사용자'
+        verbose_name_plural = '패스트캠퍼스 사용자'
